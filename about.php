304da820e5f25ff572d4b7b8a41b1ab1684493ad
kyawthiha777 <kthiha777@gmail.com>
2017-07-20 14:56:41 +0630

    Add files via upload

---
+++ about.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+		
+		<title>Kyaw Thiha | About</title>
+
+		<!-- Loading third party fonts -->
+		<link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400|" rel="stylesheet" type="text/css">
+		<link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+		<!-- Loading main css file -->
+		<link rel="stylesheet" href="style.css">
+		
+		<!--[if lt IE 9]>
+		<script src="js/ie-support/html5.js"></script>
+		<script src="js/ie-support/respond.js"></script>
+		<![endif]-->
+
+	</head>
+
+
+	<body>
+	<?php header( 'Location: /about.html' ) ;  ?> 
+		
+		<div id="site-content">
+			<div class="site-header">
+				<div class="container">
+					<a href="index.html" id="branding">
+						<img src="logo.png" alt="" class="logo">
+						<div class="logo-text">
+							<h1 class="site-title">Kyaw Thiha(K)</h1>
+							<small class="site-description">Tagline goes here</small>
+						</div>
+					</a> <!-- #branding -->
+
+					<!-- Default snippet for navigation -->
+					<div class="main-navigation">
+						<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
+						<ul class="menu">
+							<li class="menu-item"><a href="index.html">Home</a></li>
+							<li class="menu-item"><a href="news.html">Education</a></li>
+							<li class="menu-item current-menu-item"><a href="about.html">About</a></li>
+							<li class="menu-item"><a href="project.html">My Trips</a></li>
+							<li class="menu-item"><a href="contact.html">Contact</a></li>
+						</ul> <!-- .menu -->
+					</div> <!-- .main-navigation -->
+
+					<div class="mobile-navigation"></div>
+				</div>
+			</div> <!-- .site-header -->
+
+			<main class="main-content">
+				
+				<div class="page">
+					<div class="container">
+						<h2 class="entry-title">About Me.</h2>
+						
+						<p>My name is Kyaw Thiha.My parents name is U Kyaw Thein and Daw Khin Khin Hnaung.My father is Electrical Engineer and my mother is civil engineer.But my father works on the ship and my mother does house-wife.I have one brother.His name is Kyaw Kyaw Khine.He is dentist and lecturer at MIC(Myanmar Imperial College).</p>
+						
+						<figure class="align-left"><img src="my picture.jpg" alt="Single image"></figure>
+						<p>I am Kyaw Thiha.I was born in 4 July 1997.I was born on Friday morning.Now I am 20 years old.I was study my primary,secondary and high school at No.6 Basic Education High School Botahtaung.When I went to my school at my childhood age,at the break time I played football.My ambition is to be a professional computer engineer.</p>
+						
+						<p>When I finished my maticulation examination I need to choose what I will do in the future.I want to be a professional engineer so I told my parents that I want to do.So they sent me to the UniMAP(University Malaysia Perlis).Now i am studying my university life at UniMAP.When I arrived to that university i am the youngest in the university because in the foreign countries they have until 12 grades.For us we have only 10 grades.So I need to attend one course that is international english course for 6 months.And then I can join to the university.Now I already finished third year.In my university I choose computer engineering.In the courses most of the subjects are electric.In that electric it include electric cirult 1 and 2,digital electronic 1 and 2,analog electronic 1 and 2,electrical engineering.And then it also teach us engineering mathematics 1 and 2.And it also consists of C programing,microprocessor,assembly languages,embedded system and principle of computer architecture.</p>
+						
+						<p>After third year finished at my university,we need to do internship.Now I am doing my internship at mspiral creative media.I am happy to do at that job.I can learn a lot of things from them.I will do my internship until 8 of september 2017.</p>
+						
+						<hr class="separator">
+						
+						<h2 class="sectiont-title">My Hobbies</h2>
+						<div class="row">
+							<div class="col-md-3">
+								<div class="feature-numbered">
+									<div class="num">1</div>
+									<h2 class="feature-title">Playing football</h2>
+									<p>At my childhood life,most of the time I played football at the stadium</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="feature-numbered">
+									<div class="num">2</div>
+									<h2 class="feature-title">Playing games</h2>
+									<p>When I have a free times,I play games</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="feature-numbered">
+									<div class="num">3</div>
+									<h2 class="feature-title">Chatting with my friends</h2>
+									<p>At the night time I chat with my friends</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="feature-numbered">
+									<div class="num">4</div>
+									<h2 class="feature-title">Hang out with friends</h2>
+									<p>If we all have a free times,we hang out.</p>
+								</div>
+							</div>
+						</div>
+						
+						<hr class="separator">
+						
+						<h2 class="section-title">Traning and Courses</h2>
+						<div class="row">
+							<div class="col-md-4">
+								<blockquote class="testimonial">
+									<p>I studied network engineering</p>
+									<cite>Y Max ICT Professional Training Centre</cite>
+								</blockquote>
+								<blockquote class="testimonial">
+									<p>I studied C programing</p>
+									<cite>KMD Computer Training Centre</cite>
+								</blockquote>
+							</div>
+							<div class="col-md-4">
+								<blockquote class="testimonial">
+									<p>I studied ubuntu basic</p>
+									<cite>UniMAP(University Malaysia Perlis)</cite>
+								</blockquote>
+								<blockquote class="testimonial">
+									<p>I studied International English course </p>
+									<cite>UniMAP(University Malaysia Perlis)</cite>
+								</blockquote>
+							</div>
+							<div class="col-md-4">
+								<blockquote class="testimonial">
+									<p>I studied four skills</p>
+									<cite>success</cite>
+								</blockquote>
+								<blockquote class="testimonial">
+									<p>I studied photoshop</p>
+									<cite>KMD Computer Training Centre</cite>
+								</blockquote>
+							</div>
+						</div>
+					</div>
+				</div> <!-- .page -->
+
+			</main> <!-- .main-content -->
+
+			<footer class="site-footer">
+				<div class="container">
+					<div class="pull-left">
+
+						<address>
+							<strong>Kyaw Thiha</strong>
+							<p>33 137 Street, Tamwe</p>
+						</address>
+
+						<a href="#" class="phone">09796320181</a>
+					</div> <!-- .pull-left -->
+			
+				
+
+				</div> <!-- .container -->
+			</footer> <!-- .site-footer -->
+		</div>
+
+		<script src="js/jquery-1.11.1.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/app.js"></script>
+		
+	</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+		
+		<title>Kyaw Thiha</title>
+
+		<!-- Loading third party fonts -->
+		<link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400|" rel="stylesheet" type="text/css">
+		<link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+		<!-- Loading main css file -->
+		<link rel="stylesheet" href="style.css">
+		
+		<!--[if lt IE 9]>
+		<script src="js/ie-support/html5.js"></script>
+		<script src="js/ie-support/respond.js"></script>
+		<![endif]-->
+
+	</head>
+
+
+	<body>
+	<?php header( 'Location: /index.html' ) ;  ?>
+		
+		<div id="site-content">
+			<div class="site-header">
+				<div class="container">
+					<a href="index.html" id="branding">
+						<img src="logo.png" alt="" class="logo">
+						<div class="logo-text">
+							<h1 class="site-title">Kyaw Thiha(K)</h1>
+							<small class="site-description">Tagline goes here</small>
+						</div>
+					</a> <!-- #branding -->
+
+					<!-- Default snippet for navigation -->
+					<div class="main-navigation">
+						<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
+						<ul class="menu">
+							<li class="menu-item current-menu-item"><a href="index.html">Home</a></li>
+							<li class="menu-item"><a href="news.html">Education</a></li>
+							<li class="menu-item"><a href="about.html">About</a></li>
+							<li class="menu-item"><a href="project.html">My Trips</a></li>
+							<li class="menu-item"><a href="contact.html">Contact</a></li>
+						</ul> <!-- .menu -->
+					</div> <!-- .main-navigation -->
+
+					<div class="mobile-navigation"></div>
+				</div>
+			</div> <!-- .site-header -->
+
+			<div class="hero hero-slider">
+				<ul class="slides">
+					<li data-bg-image="myfamily1.jpg">
+						<div class="container">
+							<div class="slide-title">
+								<span>My Family</span> 
+				
+							</div>
+						</div>
+					</li>
+					<li data-bg-image="myfamily2.jpg">
+						<div class="container">
+							<div class="slide-title">
+								<span>My Family</span>
+							</div>
+						</div>
+					</li>
+					<li data-bg-image="mypicture5.jpg">
+						<div class="container">
+							<div class="slide-title">
+								<span>My family</span>
+							</div>
+						</div>
+					</li>
+				</ul> <!-- .slides -->
+			</div> <!-- .hero-slider -->
+
+			<main class="main-content">
+				<div class="fullwidth-block latest-projects-section">
+					<div class="container">
+						<h2 class="section-title">My latest Trips</h2>
+						<div class="row">
+							<div class="col-sm-6 col-md-3">
+								<div class="project">
+									<figure class="project-thumbnail"><img src="mypicture4.jpg" alt="Project 1"></figure>
+									<h3 class="project-title"><a href="#">At 7miles beach Mandalay</a></h3>
+									<a href="#" class="more-link"><img src="arrow.png" alt=""></a>
+								</div>
+							</div>
+							<div class="col-sm-6 col-md-3">
+								<div class="project">
+									<figure class="project-thumbnail"><img src="mypicture6.jpg" alt="Project 2"></figure>
+									<h3 class="project-title"><a href="#">At Grape Garden</a></h3>
+									<a href="#" class="more-link"><img src="arrow.png" alt=""></a>
+								</div>
+							</div>
+							<div class="col-sm-6 col-md-3">
+								<div class="project">
+									<figure class="project-thumbnail"><img src="mypicture7.jpg" alt="Project 3"></figure>
+									<h3 class="project-title"><a href="#">At 7 miles beach Mandalay</a></h3>
+									<a href="#" class="more-link"><img src="arrow.png" alt=""></a>
+								</div>
+							</div>
+							<div class="col-sm-6 col-md-3">
+								<div class="project">
+									<figure class="project-thumbnail"><img src="mypicture8.jpg" alt="Project 4"></figure>
+									<h3 class="project-title"><a href="#">The gate of Mandalay 7 miles beach</a></h3>
+									<a href="#" class="more-link"><img src="arrow.png" alt=""></a>
+								</div>
+							</div>
+						</div> <!-- .row -->
+					</div> <!-- .container -->
+				</div> <!-- .fullwidth-block.latest-projects-section -->
+
+				<div class="fullwidth-block image-block" data-bg-image="mypicture20.jpg"></div>
+
+				<div class="fullwidth-block">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-4">
+								
+								<h3 class="section-title">About Me</h3>
+								<p>I am Kyaw Thiha. </p>
+								<a href="#" class="button">Read more</a>
+								
+							</div>
+							<div class="col-md-4">
+								
+								<h3 class="section-title">Qualification</h3>
+								<p>I passed the matriculation exam.</p>
+								<a href="#" class="button">Read more</a>
+								
+							</div>
+							<div class="col-md-4">
+								
+								<h3 class="section-title">Education</h3>
+								<p>Now I am studying in UniMAP(University Malaysia Perlis).</p>
+								<a href="#" class="button">Read more</a>
+								
+							</div>
+						</div>
+
+						<hr class="separator">
+
+						<div class="row">
+							<div class="col-md-6">
+								<h2 class="section-title">Training and Courses</h2>
+								<div class="testimonial-slider">
+									<ul class="slides">
+										<li>
+											<blockquote>
+												<p>I finished network engineering</p>
+												<cite>Y Max ICT Professional Training Centre</cite>
+											</blockquote>
+										</li>
+										<li>
+											<blockquote>
+												<p>I finished C programing </p>
+												<cite>KMD Computer Training Centre</cite>
+											</blockquote>
+										</li>
+										<li>
+											<blockquote>
+												<p>I finished ubuntu basic</p>
+												<cite>UniMAP(University Malaysia Perlis)</cite>
+											</blockquote>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<h2 class="section-title">Latest News</h2>
+								<ul class="news">
+									<li>
+										<div class="date">22.7.2017</div>
+										<h3 class="entry-title"><a href="#">I will go to Bangkok</a></h3>
+									</li>
+									<li>
+										<div class="date">29.6.2017</div>
+										<h3 class="entry-title"><a href="#">I do my internship at mspiral creative media</a></h3>
+									</li>
+								</ul>
+							</div>
+						</div> <!-- .row -->
+						
+					</div> <!-- .container -->
+				</div> <!-- .fullwidth-block -->
+			</main> <!-- .main-content -->
+
+			<footer class="site-footer">
+				<div class="container">
+					<div class="pull-left">
+
+						<address>
+							<strong>Kyaw Thiha</strong>
+							<p>33 137 Street, tamwe</p>
+						</address>
+
+						<a href="#" class="phone">09796320181</a>
+					</div> <!-- .pull-left -->
+					
+
+				</div> <!-- .container -->
+			</footer> <!-- .site-footer -->
+		</div>
+
+		<script src="js/jquery-1.11.1.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/app.js"></script>
+		
+	</body>
+
+</html>
